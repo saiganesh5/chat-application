00650aa0f75019430400a03b2424ba9860ab7d14
Sai Ganesh <ganesh.12222126@lpu.in>
2026-02-01 16:07:07 +0530

    Initial commit

---
+++ src/main/java/com/chat/app/config/WebSocketConfig.java
@@ -0,0 +1,7 @@
+package com.chat.app.config;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+
+}
